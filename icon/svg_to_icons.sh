d4fdf9a4ad3da5813e647e8008fd05084083a683
Lukasz Migas <lukas.migas@yahoo.com>
2023-06-30 09:18:52 +0100

    Updated icons

--- icon/svg_to_icons.sh
+++ icon/svg_to_icons.sh
@@ -4,9 +4,12 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo $script_dir
 
 convert -background transparent -resize 2000x2000 $script_dir/icon.svg $script_dir/icon.png
+echo "Generated png icon"
 convert -background transparent $script_dir/icon.svg -define icon:auto-resize=16,24,32,48,64,72,96,128,256 $script_dir/icon.ico
+echo "Generated ico icon"
 sh $script_dir/svg_to_icns.sh $script_dir/icon.svg $script_dir/icon.icns
-cp icon.ico ../src/ims2micro/assets/icon.ico
-cp icon.icns ../src/ims2micro/assets/icon.icns
+echo "Generated icns icon"
+mv icon.ico ../src/ims2micro/assets/icon.ico
+mv icon.icns ../src/ims2micro/assets/icon.icns
+mv icon.png ../src/ims2micro/assets/icon.png
 cp icon.svg ../src/ims2micro/assets/icon.svg
-cp icon.png ../src/ims2micro/assets/icon.png
