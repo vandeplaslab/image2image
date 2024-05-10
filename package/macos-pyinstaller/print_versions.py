697303d94e3273fecedfbc8bf80341c47161a36a
Lukasz Migas <lukas.migas@yahoo.com>
2024-05-10 15:35:21 +0200

    Update print_versions.py

--- package/macos-pyinstaller/print_versions.py
+++ package/macos-pyinstaller/print_versions.py
@@ -1,5 +1,6 @@
 """Print versions."""
 import importlib
+
 for module in [
     "image2image",
     "image2image_io",
@@ -12,4 +13,3 @@ for module in [
     mod = importlib.import_module(module)
     version = mod.__version__
     print(f"{module}: {version}")
-
