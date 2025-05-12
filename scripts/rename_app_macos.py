e785adf5311e7bbfb8b256a99fe99268532f9baf
Lukasz Migas <lukas.migas@yahoo.com>
2025-05-12 10:09:58 +0100

    Improvemets

---
+++ scripts/rename_app_macos.py
@@ -0,0 +1,12 @@
+"""Rename package to represent the macOS platform + version."""
+
+import inspect
+from pathlib import Path
+
+from image2image import __version__
+
+parent = Path(inspect.getfile(lambda: None)).parent.parent.resolve()
+print("Parent:", parent)
+source_dir = parent / "dist" / "image2image.dmg"
+output_dir = parent / "dist" / f"image2image-v{__version__}-macosx_arm64.dmg"
+source_dir.rename(output_dir)
--- scripts/zip_app_win.py
+++ scripts/zip_app_win.py
@@ -1,4 +1,5 @@
 """Zip file."""
+
 import inspect
 from pathlib import Path
 from shutil import make_archive
@@ -9,7 +10,8 @@ from image2image import __version__
 
 print("Started zipping folder")
 with MeasureTimer() as timer:
-    parent = Path(inspect.getfile(lambda: None)).parent.resolve()
+    parent = Path(inspect.getfile(lambda: None)).parent.parent.resolve()
+    print("Parent directory:", parent)
     source_dir = parent / "dist" / "image2image"
     output_dir = parent / "dist" / f"image2image-v{__version__}-win_amd64"
     make_archive(output_dir, "zip", source_dir)
