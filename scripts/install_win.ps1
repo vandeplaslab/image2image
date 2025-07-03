02e18b0a1c17c0bb07f5e03b15339ec2f9838d0d
Lukasz Migas <lukas.migas@yahoo.com>
2025-07-03 04:06:30 -0700

    Create install_win.ps1

---
+++ scripts/install_win.ps1
@@ -0,0 +1,24 @@
+# Install the required packages
+uv pip install pyqt6
+uv pip install -e .
+
+# Install other packages
+cd ../image2image-io
+uv pip install -e ".[dev]"
+
+cd ../image2image-reg
+uv pip install -e .
+
+cd ../qtextra
+uv pip install -e ".[console,sentry,pyqt6]"
+
+cd ../qtextraplot
+uv pip install -e ".[2d]"
+
+cd ../qtreload
+uv pip install -e .
+
+cd ../koyo
+uv pip install -e .
+
+cd ../image2image
