33877f550b36a4cfb4a29577df919678f3d8bebb
Lukasz Migas <lukas.migas@yahoo.com>
2025-09-17 09:55:13 +0100

    Update hook-image2image.py

--- package/_hooks/hook-image2image.py
+++ package/_hooks/hook-image2image.py
@@ -1,5 +1,5 @@
 from PyInstaller.utils.hooks import collect_data_files
-import ionglow
+import image2image
 
+hiddenimports = ["image2image", "image2image_io", "image2image_reg"]
 datas = collect_data_files("image2image")
-hiddenimports = ["image2image"]
