b9904095c0384d2bc34dd6f88854294c60e0b63f
Lukasz Migas <lukas.migas@yahoo.com>
2026-06-26 11:52:15 +0100

    Move docs

---
+++ package/_runtimehooks/hook-mpl.py
@@ -0,0 +1,11 @@
+"""Matplotlib hook to avoid font cache issues."""
+
+import os
+import sys
+import atexit
+import tempfile
+
+if getattr(sys, "frozen", False):
+    cache_dir = os.path.join(tempfile.gettempdir(), "mpl_cache")
+    os.makedirs(cache_dir, exist_ok=True)
+    os.environ.setdefault("MPLCONFIGDIR", cache_dir)
